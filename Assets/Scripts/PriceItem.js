968e389bfcbb1d5ba8af327359c5cf49e5363686
isabel_x <osadaken@MacBook-Pro-Isabel.local>
2014-02-28 07:10:52 +0900

    item add

---
+++ Assets/Scripts/PriceItem.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var price = 10;
+
+private var isPickedUp = false;
+
+function OnTriggerEnter( collider: Collider ){
+
+	var status : MyCharacterStatus = collider.GetComponent(MyCharacterStatus);
+	
+	if(status == null) return;
+	
+	if(isPickedUp) return;
+	
+	status.AddPrice(price);
+	
+	Destroy(gameObject);
+}
+
+function Reset(){
+	if(collider == null){
+		gameObject.AddComponent(SphereCollider);
+	}
+	
+	collider.isTrigger = true;
+}
